b53d13dad8b7265776ae5a808742ece7cba08fd4
heerachoi <heera7722@gmail.com>
2023-01-07 22:06:07 +0900

    feat: navigation

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Root from './navigation/Root';
+import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
+const queryClient = new QueryClient();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>TEST</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='dark' />
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <Root />
+        </NavigationContainer>
+      </QueryClientProvider>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
