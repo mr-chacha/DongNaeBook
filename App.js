dbb92391d8c6162272f5c5041940baa996c7943c
prdg <prdg990515@gmail.com>
2023-01-07 14:51:58 +0900

    test 도서 상세정보 조회 UI 브랜치 생성

--- App.js
+++ App.js
@@ -3,18 +3,8 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>TEST</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
