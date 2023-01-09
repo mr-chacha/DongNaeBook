cd8afb35b6db6e970a58cacf88e0afa2ba73c6fb
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-01-09 09:33:01 +0900

    feat: reviews UI 구현

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+//
+import Detail from './screen/Detail';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>TEST</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Detail />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
