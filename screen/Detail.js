d56710b91d6c596978d9e0cd0764003d9e6ce670
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-01-09 16:06:08 +0900

    commit for merge

--- screen/Detail.js
+++ screen/Detail.js
@@ -1,12 +1,12 @@
-import styled from "@emotion/native";
-import { Rating } from "react-native-ratings";
-import { SCREEN_WIDTH, SCREEN_HEIGHT } from "../util/Dimension";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import { useState } from "react";
-import DetailContent from "../components/Detail/DetailContent";
-import { ScrollView, ActivityIndicator } from "react-native";
-import { useEffect } from "react";
+import styled from '@emotion/native';
+import { Rating } from 'react-native-ratings';
+import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../util/Dimension';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { useState } from 'react';
+import DetailContent from '../components/Detail/DetailContent';
+import { ScrollView, ActivityIndicator } from 'react-native';
+import { useEffect } from 'react';
 // npm i react-native-ratings
 
 export default function Detail({
@@ -24,18 +24,17 @@ export default function Detail({
   // 로딩 state
   // const [isLoading, setIsLoading] = useState(true);
   // 신간도서 request url
-  const BASE_URL = "http://book.interpark.com/api/newBook.api";
+  const BASE_URL = 'http://book.interpark.com/api/newBook.api';
   //  베스트 셀러 url
-  const BEST_BASE_URL = "https://book.interpark.com/api";
+  const BEST_BASE_URL = 'https://book.interpark.com/api';
 
   // api key
-  const API_KEY =
-    "CAD800FCCF43A0A4B5BAD86C45EFCBC99D6140870C5C960566AE4D254543570F";
+  const API_KEY = 'CAD800FCCF43A0A4B5BAD86C45EFCBC99D6140870C5C960566AE4D254543570F';
   //신간도서 api 가져오기
   const getApiRecentBooks = async () => {
-    const { item } = await fetch(
-      `${BASE_URL}?key=${API_KEY}&categoryId=100&output=json`
-    ).then((res) => res.json());
+    const { item } = await fetch(`${BASE_URL}?key=${API_KEY}&categoryId=100&output=json`).then(
+      (res) => res.json()
+    );
     setRecentBooks(item);
     // setIsLoading(false);
   };
@@ -75,12 +74,22 @@ export default function Detail({
       {recentBooks
         .filter((i) => i.itemId == bookId)
         .map((book) => {
-          return <DetailContent key={book.itemId} book={book} />;
+          return (
+            <DetailContent
+              key={book.itemId}
+              book={book}
+            />
+          );
         })}
       {bestSeller
         .filter((i) => i.itemId == bookId)
         .map((book) => {
-          return <DetailContent key={book.itemId} book={book} />;
+          return (
+            <DetailContent
+              key={book.itemId}
+              book={book}
+            />
+          );
         })}
 
       <Reviewcontainner>
@@ -92,17 +101,19 @@ export default function Detail({
               startingValue={0}
               ratingCount={5}
               imageSize={18}
-              type="custom"
-              ratingBackgroundColor="white"
+              type='custom'
+              ratingBackgroundColor='#d6d5d2'
               jumpValue={0.5}
+              fractions={1}
+              tintColor='#ededed'
             />
           </ReviewTitleRateBox>
           <ReviewTextInput
             maxLength={100}
             multiline={true}
-            placeholder="의견 남기기"
+            placeholder='의견 남기기'
             scrollEnabled={false}
-            onSubmitEditing={() => console.log("등록완료")}
+            onSubmitEditing={() => console.log('등록완료')}
           />
           <ReviewSubmitBtn>
             <SubmitText>등록하기</SubmitText>
@@ -113,7 +124,7 @@ export default function Detail({
           <ProfileImgBox>
             <ProfileImg
               source={{
-                uri: "https://img.extmovie.com/files/attach/images/135/286/386/076/02197f8e7c1fe5257dd98ecf223475e6.jpg",
+                uri: 'https://img.extmovie.com/files/attach/images/135/286/386/076/02197f8e7c1fe5257dd98ecf223475e6.jpg',
               }}
             />
           </ProfileImgBox>
@@ -125,42 +136,55 @@ export default function Detail({
               <CreatedDate>22.01.06</CreatedDate>
             </InfoBox>
             <Desc>
-              오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
-              읽어보고{" "}
+              오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간
+              베스트셀러여서 읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
+              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간
+              베스트셀러여서 읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
+              읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간
+              베스트셀러여서 읽어보고오랜 기간 베스트셀러여서 읽어보고오랜 기간 베스트셀러여서
+              읽어보고오랜 기간 베스트셀러여서 읽어보고{' '}
             </Desc>
           </Commentbody>
           <IconBox onPress={handleModalOpen}>
             <MaterialCommunityIcons
-              name="dots-vertical"
+              name='dots-vertical'
               size={24}
-              color="black"
+              color='black'
             />
           </IconBox>
         </CommentBox>
-        <ModifyModal visible={isModify} transparent animationType="slide">
+        <ModifyModal
+          visible={isModify}
+          transparent
+          animationType='slide'>
           <FakeView></FakeView>
           <ModifyBox>
             <MenuBox>
               <MenuWrapper>
                 <RewriteMenu>
-                  <AntDesign name="edit" size={24} color="black" />
+                  <AntDesign
+                    name='edit'
+                    size={24}
+                    color='black'
+                  />
                   <MenuName>수정하기</MenuName>
                 </RewriteMenu>
                 <DeleteMenu>
-                  <AntDesign name="delete" size={24} color="black" />
+                  <AntDesign
+                    name='delete'
+                    size={24}
+                    color='black'
+                  />
                   <MenuName>삭제하기</MenuName>
                 </DeleteMenu>
               </MenuWrapper>
 
               <CloseBox onPress={handleModalClose}>
-                <AntDesign name="close" size={24} color="black" />
+                <AntDesign
+                  name='close'
+                  size={24}
+                  color='black'
+                />
               </CloseBox>
             </MenuBox>
           </ModifyBox>
@@ -265,8 +289,7 @@ const ProfileImg = styled.Image`
   border-radius: 50%;
 `;
 
-const Commentbody = styled.View`
-`;
+const Commentbody = styled.View``;
 
 const Rate = styled.Text`
   margin-bottom: 8px;
@@ -292,7 +315,7 @@ const CreatedDate = styled.Text`
 `;
 
 const Desc = styled.Text`
-  width: ${SCREEN_WIDTH / 1.5 + "px"};
+  width: ${SCREEN_WIDTH / 1.5 + 'px'};
 `;
 
 const IconBox = styled.TouchableOpacity``;
