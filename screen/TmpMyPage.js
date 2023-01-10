71b9b27c0c4301eea56fb34985d852efb598b8a2
Yujin Son <y09urt77@gmail.com>
2023-01-10 12:08:16 +0900

    마이페이지 프로필 + 모달 + 모달배경blur주석&확인필요

--- screen/TmpMyPage.js
+++ screen/TmpMyPage.js
@@ -14,19 +14,23 @@ import { SimpleLineIcons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
+// import { Blurhash } from "react-native-blurhash";
 
 const Tab = createBottomTabNavigator();
+git;
 
 export default function TmpMyPage() {
-  const [isModify, setIsModify] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleModalOpen = () => {
-    setIsModify(true);
+    setModalVisible(true);
   };
 
   const handleModalClose = () => {
-    setIsModify(false);
+    setModalVisible(false);
   };
+
+  //모달 오픈되면 배경 블러
 
   return (
     <View>
@@ -42,24 +46,47 @@ export default function TmpMyPage() {
               style={{ flexDirection: "row", marginLeft: "auto" }}
               // onPress={}
             />
-            <ProfileImage style={{ width: 158, height: 158 }} />
+            <Image
+              style={{ width: 170, height: 170, borderRadius: 85 }}
+              source={require("../screen/image/BasicProfile.jpeg")}
+            />
             <Nickname>집요정</Nickname>
             <MyEmail> dongnaebook@gmail.com</MyEmail>
 
-            <Modal visible={isModify} style={{ width: 250, height: 300 }}>
-              <TouchableOpacity>
-                <AntDesign
-                  onPress={handleModalClose}
-                  name="close"
-                  size={24}
-                  color="black"
-                />
-              </TouchableOpacity>
-              <View>
-                <ProfileImage style={{ width: 158, height: 158 }} />
-                <Nickname>집요정</Nickname>
-                <MyEmail> dongnaebook@gmail.com</MyEmail>
-              </View>
+            {/* 모달 */}
+            <Modal
+              visible={modalVisible}
+              // transparent={true}
+              animationType="fade"
+            >
+              {/* <Blurhash
+                blurhash="LGFFaXYk^6#M@-5c,1J5@[or[Q6."
+                style={{ flex: 1 }}
+              /> */}
+              <ModalBox>
+                {/* 닫기버튼인데 모달배경컬러를 설정할 수 없어서 확인으로만 닫을 수 있게 하려고 합니다ㅠㅠ */}
+                {/* <TouchableOpacity>
+                  <AntDesign
+                    onPress={handleModalClose}
+                    name="close"
+                    size={24}
+                    color="black"
+                    style={{ flexDirection: "row", marginLeft: "auto" }}
+                  />
+                </TouchableOpacity> */}
+                <ModalProfileView>
+                  <TouchableOpacity>
+                    <ProfileImageInput
+                      style={{ width: 158, height: 158, borderRadius: 79 }}
+                      source={require("../screen/image/BasicProfile.jpeg")}
+                    />
+                  </TouchableOpacity>
+                  <NicknameInput>집요정</NicknameInput>
+                  <EditButton onPress={handleModalClose}>
+                    <Text>확인</Text>
+                  </EditButton>
+                </ModalProfileView>
+              </ModalBox>
             </Modal>
           </MypageContainer>
         </ScrollView>
@@ -68,14 +95,50 @@ export default function TmpMyPage() {
   );
 }
 
-{
-  /* 프로필이미지 */
-}
-const ProfileImage = styled.TouchableOpacity`
-  width: 158px;
-  height: 158px;
-  border-radius: 79px;
-  background-color: grey;
+// 모달
+const ModalBox = styled.View`
+  margin: auto;
+  width: 300;
+  height: 350px;
+  border-radius: 15px;
+  background-color: white;
+  padding: 10px;
+
+  /* justify-content: center; */
+  /* align-items: center; */
+`;
+
+// 프로필(프사+닉네임) in 모달
+const ModalProfileView = styled.View`
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+// 프로필input in 모달
+const ProfileImageInput = styled.Image``;
+
+// 수정완료버튼 in 모달
+const EditButton = styled.TouchableOpacity`
+  width: 90px;
+  height: 30px;
+  background-color: #cdff40;
+  border-radius: 15px;
+  align-items: center;
+  margin-top: 20px;
+  text-align: center;
+  padding: 5px;
+`;
+
+const NicknameInput = styled.TextInput`
+  width: 116px;
+  height: 30px;
+  border: 2px solid #cdff40;
+  border-radius: 15px;
+  align-items: center;
+  margin-top: 20px;
+  text-align: center;
+  padding: 5px; // 텍스트 세로정렬 padding말고 다른방법 아는분 계실까요?ㅠ
 `;
 
 //마이페이지 전체 뷰
