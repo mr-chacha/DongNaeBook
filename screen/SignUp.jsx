764caba47894840a8e64d5789815a135c7f98a6c
heerachoi <heera7722@gmail.com>
2023-01-11 15:57:01 +0900

    fix: 회원가입 페이지 에러 수정

--- screen/SignUp.jsx
+++ screen/SignUp.jsx
@@ -1,80 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
-import { useFonts } from 'expo-font';
-import styled from '@emotion/native';
-import { v4 as uuidv4 } from 'uuid';
-
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { authService, db } from '../firebase';
-import { collection, setDoc, doc } from 'firebase/firestore';
+import { authService } from '../firebase';
+import styled from '@emotion/native';
+
+import { TouchableOpacity, Text, TextInput, Button, Alert } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useNavigation } from '@react-navigation/native';
+import { AuthInput } from '../components/Auth/AuthInput.js';
 
 export default function SignUp({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [disabled, setDisabled] = useState('');
+  const { goBack, setOptions } = useNavigation();
+
   // 폰트
   const [isFontLoaded] = useFonts({
     PyeongChangPeace: require('../assets/fonts/PyeongChangPeace-Bold.otf'),
   });
+
   if (!isFontLoaded) {
     return null;
   }
 
-  // 초기값 세팅
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [nickName, setNickName] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  // 오류메세지 상태 저장
-  const [nickNameMessage, setNickNameMessage] = useState('');
-  const [emailMessage, setEmailMessage] = useState('');
-  const [passwordMessage, setPasswordMessage] = useState('');
-  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
-
-  // 유효성 검사
-  const [isNickName, setIsNickName] = useState(false);
-  const [isEmail, setIsEmail] = React.useState(false);
-  const [isPassword, setIsPassword] = useState(false);
-  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
-
-  // 버튼 활성화
-  const [disabled, setDisabled] = useState(true);
-  const { goBack, setOptions } = useNavigation();
-
-  // 회원가입 버튼
+  // 회원가입 또는 로그인 버튼
   const handleAuthentication = () => {
-    const generateId = uuidv4();
-    const usersRef = collection(db, 'users');
-
     // 회원가입 요청
     createUserWithEmailAndPassword(authService, email, password)
       .then(() => {
-        alert('동네북 회원이 되신걸 환영합니다!');
-        setDoc(doc(usersRef, generateId), {
-          nickName: nickName,
-          id: generateId,
-          uid: authService.currentUser.uid,
-          email: email,
-        });
-        navigation.navigate('Home');
+        console.log('성공!');
       })
       .catch((err) => {
         console.log(err.message);
-        alert('이미 존재하는 계정 입니다.');
+        alert(err.message);
       });
   };
-
-  //* 회원가입 버튼 활성화
-  useEffect(() => {
-    if (isNickName && isEmail && isPassword && isPasswordConfirm) {
-      setDisabled(false);
-      return;
-    }
-    setDisabled(true);
-  }, [isNickName, isEmail, isPassword, isPasswordConfirm]);
-
+  // console.log(authService);
   const handleEmailChange = (email) => {
     const changedEmail = removeWhitespace(email);
     setEmail(changedEmail);
@@ -84,15 +48,22 @@ export default function SignUp({ navigation }) {
   const handlePasswordChange = (password) => {
     setPassword(removeWhitespace(password));
   };
-
+  // <UserInfoInput
+  //   placeholder='비밀번호 확인'
+  //   placeholderTextColor='#d4d4d4'
+  //   autoCapitalize='none'
+  //   autoCorrect={false}
+  //   secureTextEntry={true}
+  //   textContentType='password'
+  //   value={confirmPassword}
+  //   onChange={(text) => setConfirmPassword(text)}
+  // />;
   return (
     <SafeAreaView>
       <AuthenticationContainer>
         <ApplicationTitle>동네북</ApplicationTitle>
-        <UserInfoInput placeholder='닉네임' placeholderTextColor='#d4d4d4' autoCapitalize='none' value={nickName} onChangeText={setNickName} />
         <UserInfoInput placeholder='donnaebook@gmail.com' placeholderTextColor='#d4d4d4' autoCapitalize='none' value={email} onChangeText={setEmail} />
-        <UserInfoInput placeholder='비밀번호 입력' autoCapitalize='none' placeholderTextColor='#d4d4d4' value={password} onChangeText={setPassword} secureTextEntry={true} />
-        <UserInfoInput placeholder='비밀번호 확인' autoCapitalize='none' placeholderTextColor='#d4d4d4' value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry={true} />
+        <UserInfoInput placeholder='비밀번호 입력' placeholderTextColor='#d4d4d4' value={password} onChangeText={setPassword} />
         <AuthenticationFormSubmitButton onPress={handleAuthentication}>
           <Text>회원가입</Text>
         </AuthenticationFormSubmitButton>
