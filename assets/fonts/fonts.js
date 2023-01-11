9582d204903b08cdbc2539f1c6a0832e9045cf44
prdg <prdg990515@gmail.com>
2023-01-11 16:21:14 +0900

    delete all

--- assets/fonts/fonts.js
+++
@@ -1,9 +0,0 @@
-// const [isReady, setIsReady] = useEffect(false);
-
-// useEffect(async () => {
-//   await Font.loadAsync({
-//     fredoka: require('./assets/fonts/FredokaOne-Regular.ttf'),
-//     cafe24: require('./assets/fonts/Cafe24Ssurround.ttf'),
-//   });
-//   setIsReady(true);
-// }, []);
