9f77cad24c2e9234f51481c2d32e7289bcc45c1d
heerachoi <heera7722@gmail.com>
2023-01-12 09:23:06 +0900

    feat: 회원가입 유효성 검사

--- navigation/Root.js
+++ navigation/Root.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { StatusBar } from 'expo-status-bar';
-
-import { createContext, useEffect, useState, useContext } from 'react';
+import { QueryClient, Button, Alert } from 'react-query';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Tabs from './Tabs';
 import Login from '../screen/Login';
 import SignUp from '../screen/SignUp';
-import TmpHome from '../screen/TmpHome';
-import { View, ActivityIndicator } from 'react-native';
-import { onAuthStateChanged } from 'firebase/auth';
-import { authService } from '../firebase';
-import Stacks from './Stacks';
 import Detail from '../screen/Detail';
 
 const queryClient = new QueryClient();
