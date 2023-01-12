82ac77f58c16cc7229edc14a055589116f25691f
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-01-12 14:15:04 +0900

    fix : tmpHome.js error

--- screen/TmpHome.js
+++ screen/TmpHome.js
@@ -1,27 +1,27 @@
+import React, { useState } from 'react';
+import { StyleSheet, Image, useColorScheme, Text } from 'react-native';
+import BestSeller from '../components/Home/BestSeller';
+import MonthBook from '../components/Home/MonthBook';
+import Banner from '../components/Home/Banner';
+import styled from '@emotion/native';
+import logo from '../assets/logo.png';
+import { useFonts } from 'expo-font';
+import { View } from 'react-native';
 
-import React, { useState } from "react";
-import { StyleSheet, Image, useColorScheme, Text } from "react-native";
-import BestSeller from "../components/Home/BestSeller";
-import MonthBook from "../components/Home/MonthBook";
-import Banner from "../components/Home/Banner";
-import styled from "@emotion/native";
-import logo from "../assets/logo.png";
-
-import CategoryList2 from "../components/Home/CategoryList2";
-import Switch from "expo-dark-mode-switch";
+import CategoryList2 from '../components/Home/CategoryList2';
+import Switch from 'expo-dark-mode-switch';
 export default function TmpHome() {
   // 폰트
   const [isFontLoaded] = useFonts({
-    PyeongChangPeace: require("../assets/fonts/PyeongChangPeace-Bold.otf"),
+    PyeongChangPeace: require('../assets/fonts/PyeongChangPeace-Bold.otf'),
   });
 
   if (!isFontLoaded) {
     return null;
   }
   return (
-    <Safe>
+    <View>
       <Container showsVerticalScrollIndicator={false}>
-    
         <ApplicationTitle>동네북</ApplicationTitle>
 
         <Banner />
@@ -32,25 +32,23 @@ export default function TmpHome() {
         {/* 카테고리 */}
         <CategoryList2 />
       </Container>
-    </Safe>
+    </View>
   );
 }
 
-
-const Safe = styled.SafeAreaView``;
+// const Safe = styled.SafeAreaView``;
 const ApplicationTitle = styled.Text`
   font-size: 35px;
   margin-bottom: 5px;
   text-align: center;
-  font-family: "PyeongChangPeace";
+  font-family: 'PyeongChangPeace';
 `;
 
-
-const Safe = styled.View`
-  background-color: #fff;
-  margin: auto;
-  padding-top: 50px;
-`;
+// const Safe = styled.View`
+//   background-color: #fff;
+//   margin: auto;
+//   padding-top: 50px;
+// `;
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.back};
